d75eea8dcc54bd8e0bdb9c21231531aff9fef8c7
Aeoconor <46606033+Aeoconor@users.noreply.github.com>
2024-08-03 23:24:52 -0500

    home logo return button

--- pages/pages.js
+++ pages/pages.js
@@ -1,17 +1,16 @@
 // select all desc-col elements
 const descColList = document.querySelectorAll(".desc-col")
 descColList.forEach((element) => { // for each, add the custom ebay sold link
-    console.log(element)
-    console.log(element.firstElementChild)
     combinedSetCard = element.lastElementChild.textContent.concat(" " + element.firstElementChild.textContent)
     searchableText = combinedSetCard.replaceAll(" ", "+")
     element.firstElementChild.href = `https://www.ebay.com/sch/i.html?_nkw=${searchableText}&LH_Sold=1&LH_Complete=1`
-    console.log(element.firstElementChild)
 })
 
 // image click to open in new tab
 $('img').click(function(e){
     var img = $(e.target);
     var src = img.attr("src");
-    window.open(src);
+    if (e.target.className != 'home-logo') {
+        window.open(src);
+    }
 });
