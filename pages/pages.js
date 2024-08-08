53e695a5028770379f48023ac5b2e871ce073004
Aeoconor <46606033+Aeoconor@users.noreply.github.com>
2024-08-07 20:32:23 -0500

    replaced images and updated search

--- pages/pages.js
+++ pages/pages.js
@@ -60,10 +60,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const cardSearchElement = document.getElementById("card-search")
     cardSearchElement.addEventListener('input', () => {
         const sideBarDivs = document.querySelectorAll(".sidebarDiv")
+        const imgDescRows = document.querySelectorAll(".image-desc-row")
         const regx = new RegExp(`^${cardSearchElement.value.toUpperCase()}`)
         sideBarDivs.forEach((element) => {
             if (regx.test(element.textContent.toUpperCase())) {
                 element.style.display = 'block'
+            } else {
+                element.style.display = 'none'
+            }
+        })
+        imgDescRows.forEach((element) => {
+            if (regx.test(element.lastElementChild.firstElementChild.textContent.toUpperCase())) {
+                element.style.display = 'flex'
             } else {
                 element.style.display = 'none'
             }
